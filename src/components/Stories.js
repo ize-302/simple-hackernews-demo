ef999b89240ec598c2bcbc28628f3c855377ce70
ozorku <adavizeozorku@gmail.com>
2021-10-11 12:58:27 +0100

    add loading indicator

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -5,21 +5,31 @@ import Story from "./Story";
 const Stories = () => {
   const [count, setcount] = React.useState(20);
   const [storyIds, setStoryIds] = React.useState([]);
+  const [isLoading, setisLoading] = React.useState(true);
 
   React.useEffect(() => {
-    getStoryIds().then((data) => setStoryIds(data));
+    getStoryIds().then((data) => {
+      setStoryIds(data);
+      setisLoading(false);
+    });
   }, []);
 
   return (
     <>
-      <div className="stories">
-        {storyIds.slice(0, count).map((storyId, index) => (
-          <Story key={storyId} index={index} storyId={storyId} />
-        ))}
-        <div className="button__wrapper">
-          <button onClick={() => setcount(count + 10)}>More stories</button>
+      {isLoading ? (
+        <div className="loader">
+          <img src="loading.gif" alt="loading.gif" />
         </div>
-      </div>
+      ) : (
+        <div className="stories">
+          {storyIds.slice(0, count).map((storyId, index) => (
+            <Story key={storyId} index={index} storyId={storyId} />
+          ))}
+          <div className="button__wrapper">
+            <button onClick={() => setcount(count + 10)}>More stories</button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
